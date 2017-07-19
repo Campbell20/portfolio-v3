3e90c98a06b2930475923489aac9ce11f6dc5ad4
John Campbell <johncampbell5050@gmail.com>
2017-07-19 03:59:27 +0000

    added animations

--- scripts/3dart.js
+++ scripts/3dart.js
@@ -1,44 +1,52 @@
-$(window).scroll(function(){
-    // This is then function used to detect if the element is scrolled into view
-    function elementScrolled(elem)
-    {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-    }
-     
-    // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
-    if(elementScrolled('.art-1')) {
-        var els = $('.art-1'),
-            i = 0,
-            f = function () {
-                $(els[i++]).addClass('bounceInLeft');
-                if(i < els.length) setTimeout(f, 400);
-            };
-        f();
-    }
+
+$(window).scroll(function() {
+   var hT = $('.art-1, .art-2').offset().top,
+       hH = $('.art-1, .art-2').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+      $(".art-1, .art-2").addClass('flipInX')
+      $('.art-1, .art-2').removeClass('flipOutX')
+   } else {
+      $('.art-1, .art-2').removeClass('flipInX')
+      $('.art-1, .art-2').addClass('flipOutX')
+   }
 });
 
 
-$(window).scroll(function(){
-    // This is then function used to detect if the element is scrolled into view
-    function elementScrolled(elem)
-    {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-    }
-     
-    // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
-    if(elementScrolled('.art-2')) {
-        var els = $('.art-2'),
-            i = 0,
-            f = function () {
-                $(els[i++]).addClass('bounceInRight');
-                if(i < els.length) setTimeout(f, 400);
-            };
-        f();
-    }
+
+$(window).scroll(function() {
+   var hT = $('.row-1').offset().top,
+       hH = $('.row-1').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+      $(".row-1").addClass('bounceInLeft')
+   } else {
+      $('.row-1').removeClass('bounceInLeft')
+   }
+});
+
+$(window).scroll(function() {
+   var hT = $('.row-2').offset().top,
+       hH = $('.row-2').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+      $(".row-2").addClass('bounceInUp')
+   } else {
+      $('.row-2').removeClass('bounceInUp')
+   }
+});
+
+$(window).scroll(function() {
+   var hT = $('.row-3').offset().top,
+       hH = $('.row-3').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+      $(".row-3").addClass('bounceInRight')
+   } else {
+      $('.row-3').removeClass('bounceInRight')
+   }
 });
